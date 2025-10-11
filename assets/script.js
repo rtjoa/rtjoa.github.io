3112f535afbaacc54587890d4aff8d7c3100f5ac
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2025-10-11 14:23:40 +0800

    update

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    const s = "sukpbAdt/xbtijohupo/fev";
+    const s = "szboAsukpb/dpn";
     const email = String.fromCharCode(...s.split('').map(c => c.charCodeAt(0) - 1));
     document.getElementById("email").innerHTML = email;
     document.getElementById("email").href = `mailto:${email}`;
